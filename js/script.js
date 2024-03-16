f32d74c84e0f7229cffb56d887c171f4f0c8788e
Luiz Inaldo <luiz.inaldo970@gmail.com>
2024-03-16 20:02:50 -0300

    fix: css e responsividade

--- js/script.js
+++ js/script.js
@@ -1,47 +1,58 @@
 import projects from "./projects.js";
 
-const projectDiv = document.getElementById('projects_box')
+window.addEventListener('DOMContentLoaded', engage);
 
-for (let i = 0; i < projects.length; i++) {
-    
-    if (!projects[i].released) {
+async function engage() {
+
+    // mostrando o menu hamburguer (responsividade mobile)
+    document.getElementById('menu').addEventListener('click', () => {
+        document.querySelector('.navbar_links').classList.toggle('active');
+    })
+
+    // renderizando os projetos na tela
+    const projectDiv = document.getElementById('projects_box')
+    for (let i = 0; i < projects.length; i++) {
         
-        projectDiv.innerHTML += `
+        if (!projects[i].released) {
+            
+            projectDiv.innerHTML += `
+            
+                <div class="project_card">
         
-            <div class="project_card">
-    
-                <img src="${projects[i].img}" alt="megaman_repo" class="card_img">
-                <h3 class="card_title">${projects[i].titulo}</h3>
-                <p class="card_description">${projects[i].descricao}</p>
-    
-                <div class="card_links">
-                    <a target="_blank" href="${projects[i].linkGitHub}">Link GitHub</a>
+                    <img src="${projects[i].img}" alt="megaman_repo" class="card_img">
+                    <h3 class="card_title">${projects[i].titulo}</h3>
+                    <p class="card_description">${projects[i].descricao}</p>
+        
+                    <div class="card_links">
+                        <a target="_blank" href="${projects[i].linkGitHub}">Link GitHub</a>
+                    </div>
+        
                 </div>
+            
+            `;
     
-            </div>
+        } else {
+    
+            projectDiv.innerHTML += `
+            
+                <div class="project_card">
         
-        `;
-
-    } else {
-
-        projectDiv.innerHTML += `
+                    <img src="${projects[i].img}" alt="megaman_repo" class="card_img">
+                    <h3 class="card_title">${projects[i].titulo}</h3>
+                    <p class="card_description">${projects[i].descricao}</p>
         
-            <div class="project_card">
-    
-                <img src="${projects[i].img}" alt="megaman_repo" class="card_img">
-                <h3 class="card_title">${projects[i].titulo}</h3>
-                <p class="card_description">${projects[i].descricao}</p>
-    
-                <div class="card_links">
-                    <a target="_blank" href="${projects[i].linkGitHub}">Link GitHub</a>
-                    <a target="_blank" href="${projects[i].linkVercel}">Link Vercel</a>
+                    <div class="card_links">
+                        <a target="_blank" href="${projects[i].linkGitHub}">Link GitHub</a>
+                        <a target="_blank" href="${projects[i].linkVercel}">Link Vercel</a>
+                    </div>
+        
                 </div>
+            
+            `;
     
-            </div>
+        }
         
-        `;
-
+        
     }
-    
-    
+
 }
